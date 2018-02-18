78b92f3f13d76e28ce31d77577ea98856e14a013
zitochat <zitochat@gmail.com>
2018-02-18 17:09:30 +0700

    fix

--- resources/views/frontend/layouts/seo.blade.php
+++ resources/views/frontend/layouts/seo.blade.php
@@ -56,19 +56,8 @@
     $(function() {
         $('nav#menu-mb-ll').mmenu();
     });
-</script>
-       <script type="text/javascript"
-src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDIcgayYKPPDnRhRPUdgsCi63XC3-VB12k">
-</script>
-    <script>              new WOW().init();              </script> 
- 	<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-84640906-1', 'auto');
-  ga('send', 'pageview');
-
-</script>    <h1 class="vcard fn" style="display:none;"></h1>
+    new WOW().init();
+</script> 
+<h1 class="vcard fn" style="display:none;"></h1>
     <h2 style="display:none;"></h2>
